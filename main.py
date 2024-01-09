57801c406fb4ca51b6f0298efbf7521e49628a92
Monsalvo Geoffrey <monsalvo.geoffrey@gmail.com>
2024-01-09 22:33:45 +0100

    Write simple grammar

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
 import re
 
 
-def split_all(input_list: list | str, split_string: str):
-    if isinstance(input_list, str):
-        return input_list.split(split_string)
-    result = []
-    for item in input_list:
-        result.append(split_all(item, split_string))
-    return result
-
-
-#
-def split_in_sequence(input_item, *split_strings):
-    for string in split_strings:
-        input_item = split_all(input_item, string)
-    return input_item
-
-
 class Tokenizer:
     def __init__(self, language: str):
-        # self.language = split_in_sequence(language, "\n", ":")
         self.language = [
             line.split(":", maxsplit=1) for line in language.split("\n") if line != ""
         ]
@@ -29,17 +12,13 @@ class Tokenizer:
     def tokenize(self, code: str):
         tokens = []
         while (code := code.lstrip()) != "":
-            # print(code)
             for token in self.language:
-                # print(token)
                 result = token[1].match(code)
                 if result:
                     tokens.append((token[0], result.group(0)))
                     code = code[len(result.group(0)) :]
-                    # print(token[0], result.group(0), len(result.group(0)), code)
                     break
             else:
-                # print(code)
                 raise RuntimeError("Something's wrong")
         return tokens
 
